54ca456d3e99b8d93fa0309d2a6f1dfdf29ecbde
= <=>
2018-04-22 17:31:00 +0300

    changed the let declarations of the global to this file variables to const

--- js/bouncingBalls.js
+++ js/bouncingBalls.js
@@ -1,13 +1,13 @@
 // setup canvas
-let canvas = document.querySelector('canvas');
+const canvas = document.querySelector('canvas');
 
 // getContext: gives a context on which we can start drawing
 // ctx: is the object that directly represents the drawing area of the canvas
 // and allows us to draw 2D shapes on it
-let ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
 
-let width = canvas.width = window.innerWidth;
-let height = canvas.height = window.innerHeight;
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
 
 // function to generate random number
 function random(min,max) {
