e4b8c2b9621dab51ab8333f106b6d584a9403561
= <=>
2018-04-22 17:11:19 +0300

    created the constructor function for Ball

--- js/bouncingBalls.js
+++ js/bouncingBalls.js
@@ -14,3 +14,19 @@ function random(min,max) {
   let num = Math.floor(Math.random()*(max-min)) + min;
   return num;
 }
+
+function Ball(x, y, velX, velY, color, size) {
+
+    // the horizontal and vertical coordinates where the ball will start on the screen
+    this.x      = x;
+    this.y      = y;
+
+    // horizontal and vertical velocity, these values will be regularly added
+    // to the x/y coordinate values when I start to animate the balls,
+    // to move them by this much on each frame.
+    this.velX   = velX;
+    this.velY   = velY;
+
+    this.color  = color;
+    this.size   = size
+}
