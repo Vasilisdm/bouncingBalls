6ae8bb2dc000d6587cc77d4c5470c1dbfe382dad
= <=>
2018-04-22 16:58:14 +0300

    added some explenatory comments on getContext and its returned value

--- js/bouncingBalls.js
+++ js/bouncingBalls.js
@@ -1,13 +1,15 @@
 // setup canvas
-
 var canvas = document.querySelector('canvas');
+
+// getContext: gives a context on which we can start drawing
+// ctx: is the object that directly represents the drawing area of the canvas
+// and allows us to draw 2D shapes on it
 var ctx = canvas.getContext('2d');
 
 var width = canvas.width = window.innerWidth;
 var height = canvas.height = window.innerHeight;
 
 // function to generate random number
-
 function random(min,max) {
   var num = Math.floor(Math.random()*(max-min)) + min;
   return num;
