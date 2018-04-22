b40e73768aac2832b3c1e9cc4e3a120860e0ca15
= <=>
2018-04-22 17:01:34 +0300

    changed every var decleration to let

--- js/bouncingBalls.js
+++ js/bouncingBalls.js
@@ -1,13 +1,13 @@
 // setup canvas
-var canvas = document.querySelector('canvas');
+let canvas = document.querySelector('canvas');
 
 // getContext: gives a context on which we can start drawing
 // ctx: is the object that directly represents the drawing area of the canvas
 // and allows us to draw 2D shapes on it
-var ctx = canvas.getContext('2d');
+let ctx = canvas.getContext('2d');
 
-var width = canvas.width = window.innerWidth;
-var height = canvas.height = window.innerHeight;
+let width = canvas.width = window.innerWidth;
+let height = canvas.height = window.innerHeight;
 
 // function to generate random number
 function random(min,max) {
